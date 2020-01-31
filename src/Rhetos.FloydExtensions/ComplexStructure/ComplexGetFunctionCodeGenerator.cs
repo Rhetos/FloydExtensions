cb6a1f68cc77f8d200eba52c18494f5706b1237e
Damir <ddean@omega-software.hr>
2020-01-31 14:33:20 +0100

    createComplexInfo, createComplexGetInfo - part 2

---
+++ src/Rhetos.FloydExtensions/ComplexStructure/ComplexGetFunctionCodeGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Composition;
+using System.Linq;
+using Rhetos.Compiler;
+using Rhetos.ComplexEntity.Function;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace Rhetos.FloydExtensions
+{
+	[Export(typeof(ITypeScriptGeneratorPlugin))]
+	[ExportMetadata(MefProvider.Implements, typeof(ComplexGetFunctionInfo))]
+	public class ComplexGetFunctionCodeGenerator : ITypeScriptGeneratorPlugin
+	{
+		private readonly IDslModel _dslModel;
+
+		public ComplexGetFunctionCodeGenerator(IDslModel dslModel)
+		{
+			_dslModel = dslModel;
+		}
+
+		public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+		{
+			var info = (ComplexGetFunctionInfo)conceptInfo;
+
+			if (_dslModel.FindByType<ComplexSaveFunctionInfo>().Any(x => x.ComplexStructure == info.ComplexStructure))
+				return;
+
+			codeBuilder.InsertCode($@"
+    export const {info.ComplexStructure.Name}ComplexInfo = createComplexGetInfo({info.ComplexStructure.Name}GetFunctionInfo);
+", ModuleCodeGenerator.MembersTag, info.ComplexStructure.Module);
+		}
+	}
+}
---
+++ src/Rhetos.FloydExtensions/ComplexStructure/ComplexSaveFunctionCodeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.ComplexEntity.Function;
+using Rhetos.Dsl;
+using Rhetos.Extensibility;
+
+namespace Rhetos.FloydExtensions
+{
+	[Export(typeof(ITypeScriptGeneratorPlugin))]
+	[ExportMetadata(MefProvider.Implements, typeof(ComplexSaveFunctionInfo))]
+	public class ComplexSaveFunctionCodeGenerator : ITypeScriptGeneratorPlugin
+	{
+		public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+		{
+			var info = (ComplexSaveFunctionInfo)conceptInfo;
+
+			codeBuilder.InsertCode($@"
+    export const {info.ComplexStructure.Name}ComplexInfo = createComplexInfo({info.ComplexStructure.Name}GetFunctionInfo, {info.ComplexStructure.Name}SaveFunctionInfo);
+", ModuleCodeGenerator.MembersTag, info.ComplexStructure.Module);
+		}
+	}
+}
